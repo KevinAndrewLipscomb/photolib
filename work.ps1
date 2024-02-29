44ea7f6203a92e49a71d724542d24d8839d2f1fe
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2024-02-29 16:51:08 -0500

    Simplified opening of File Explorer in work.ps1.

--- work.ps1
+++ work.ps1
@@ -1,5 +1,5 @@
 Set-Location "C:\Inetpub\wwwroot\photolib"
-Start-Process -WindowStyle Maximized "C:\Inetpub\wwwroot\photolib"
+Start-Process -WindowStyle Maximized "."
 Start-Process -WindowStyle Maximized photolib.sln
 IF (Test-Path "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe")
   {
